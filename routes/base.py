13f35a376a2a7b6657963a0746f1ba4120244f74
Mostafa <mostafa.abdelraheem.ai@gmail.com>
2025-06-20 22:33:43 +0300

    add base router and welcome endpoint; include dotenv in main application

---
+++ routes/base.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+)
+
+@base_router.get("/")
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = "1.0.0"   
+    return {
+        "message": f"Welcome to {app_name} API",
+        "version": app_version
+    }
