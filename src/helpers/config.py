d77e4372c1868ed43f3a8f551ee76d92a5ac872d
Mostafa <mostafa.abdelraheem.ai@gmail.com>
2025-12-22 04:59:59 +0200

    Add configuration for PGVector support and update settings structure

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class Settings(BaseSettings):
 
@@ -11,7 +12,7 @@ class Settings(BaseSettings):
     FILE_DEFAULT_CHUNK_SIZE: int
 
     POSTGRES_USERNAME: str
-    POSTGRES_PASSWORD: str  
+    POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_PORT: int
     POSTGRES_MAIN_DATABASE: str
@@ -23,6 +24,7 @@ class Settings(BaseSettings):
     OPENAI_API_URL: str = None
     COHERE_API_KEY: str = None
 
+    GENERATION_MODEL_ID_LITERAL: List[str] = None
     GENERATION_MODEL_ID: str = None
     EMBEDDING_MODEL_ID: str = None
     EMBEDDING_MODEL_SIZE: int = None
@@ -30,9 +32,11 @@ class Settings(BaseSettings):
     GENERATION_DAFAULT_MAX_TOKENS: int = None
     GENERATION_DAFAULT_TEMPERATURE: float = None
 
+    VECTOR_DB_BACKEND_LITERAL: List[str] = None
     VECTOR_DB_BACKEND : str
     VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
+    VECTOR_DB_PGVEC_INDEX_THRESHOLD: int = 100
 
     PRIMARY_LANG: str = "en"
     DEFAULT_LANG: str = "en"
