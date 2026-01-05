d46203f012adaf572114bd6a44aff4b5e77160b4
Mostafa <mostafa.abdelraheem.ai@gmail.com>
2026-01-05 03:04:53 +0200

    Add metrics setup to FastAPI and update dependencies in requirements

--- src/main.py
+++ src/main.py
@@ -7,9 +7,14 @@ from stores.llm.templates.template_parser import TemplateParser
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+from utils.metrics import setup_metrics
+
 app = FastAPI()
 
+setup_metrics(app)
+
 async def startup_span():
+
     settings = get_settings()
 
     postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
